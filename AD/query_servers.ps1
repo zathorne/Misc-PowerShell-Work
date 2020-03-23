382c5606e768536ae091f38279b8ceeae02b038f
Zach Thorne <33821072+zathorne@users.noreply.github.com>
2020-03-23 14:29:47 -0500

    Update query_servers.ps1

--- AD/query_servers.ps1
+++ AD/query_servers.ps1
@@ -1,2 +1,6 @@
-Get-ADComputer -Filter * -SearchBase "OU=Servers, DC=DOM1, DC=domain, DC=com" -Properties name, i
-pv4address | select-object Name, dnshostname, ipv4address | export-csv -path c:\scripts\dom1output.csv -encoding utf8
+# Script: rolegather.ps1
+# Author: Zach Thorne
+# Date 3-17-2020
+# Purpose: Gather all computer objects in the server OU and export name, hostname, and IP to CSV.
+Get-ADComputer -Filter * -SearchBase "OU=Servers, DC=DOM1, DC=domain, DC=com" -Properties name, 
+ipv4address | select-object Name, dnshostname, ipv4address | export-csv -path c:\scripts\dom1output.csv -encoding utf8
